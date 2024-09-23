3b08fbfc4dbf57e7a7c582022e92126f477d7705
Vansh Sharma <xplore.vansh@gmail.com>
2024-09-23 22:19:08 +0530

    Add files via upload

---
+++ Gauss Laguerre.py
@@ -0,0 +1,71 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+def func_original(x): 
+    return np.cos(x)*np.exp(-x)
+
+def func_transform(t): 
+    x=(1+t)/(1-t)
+    derivative=2/(1-t)**2
+    return func_original(x)*derivative 
+
+def gauss_legendre_quadrature(n): 
+    if n == 2: 
+        roots=np.array([-1/np.sqrt(3),1/np.sqrt(3)])
+        weights=np.array([1,1])
+    elif n == 3: 
+        roots=np.array([-np.sqrt(3/5),0,np.sqrt(3/5)])
+        weights=np.array([5/9,8/9,5/9])
+    else: 
+        raise ValueError('Try again for n=2 & 3 only')
+    
+    return roots,weights 
+
+def gauss_legendre_integration(n): 
+    roots,weights=gauss_legendre_quadrature(n)
+    
+    results=[]
+    for i in range(n):
+        results.append(weights[i]*func_transform(roots[i]))
+
+    integral_legendre=sum(results)
+    return integral_legendre
+
+n_pt=3
+
+result_gauss_legendre=gauss_legendre_integration(n_pt)
+
+def gauss_laguerre_quadrature(n): 
+    roots,weights=np.polynomial.laguerre.laggauss(n)
+
+    integral=np.sum(weights*np.cos(roots))
+
+    return integral 
+
+result_gauss_laguerre=gauss_laguerre_quadrature(10)
+
+print(f'Result using Gauss-Legendre Transform Method is {result_gauss_legendre}')
+print(f'Result using Gauss-Laguerre Original Method is {result_gauss_laguerre}')
+
+error=abs(result_gauss_legendre-result_gauss_laguerre)
+print(f'Error between these method is {error}')
+
+# Plot the functions
+t_values=np.linspace(-1,1,500)
+x_values=np.linspace(0,5,500)
+
+original_values=func_original(x_values)
+
+plt.plot(x_values,original_values,color='blue',label=f'Original function and Domain of x∈[0,∞)')
+plt.fill_between(x_values,original_values,color='lightblue',label=f'Area under the Original function is {result_gauss_laguerre:.3f}')
+
+transform_values=func_transform(t_values)
+plt.plot(t_values,transform_values,color='green',label='Transform function and Domain of t∈[-1, 1]')
+plt.fill_between(t_values,transform_values,color='lightgreen',label=f'Area under the Transform function is {result_gauss_legendre:.3f}')
+
+plt.title('Compare of Original & Transform Function by Gauss-Laguerre Method',fontsize=10)
+plt.xlabel('x,t values')
+plt.ylabel('Function Values')
+plt.grid()
+plt.legend() 
+plt.show() 
